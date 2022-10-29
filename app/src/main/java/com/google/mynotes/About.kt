1fe4d1cf4a70e459d9ccd7fe3ea5311d56f9180b
Suryansh Prajapati <suryansh7202@gmail.com>
2022-10-29 14:23:30 +0530

    about activity create

--- app/src/main/java/com/google/mynotes/About.kt
+++ app/src/main/java/com/google/mynotes/About.kt
@@ -2,10 +2,22 @@ package com.google.mynotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.mynotes.databinding.ActivityAboutBinding
 
 class About : AppCompatActivity() {
+    private var binding:ActivityAboutBinding?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+
+        binding?.tvAbout?.setText("This is the Poem application in which you can store your application.")
+
+        binding?.btnAboutBack?.setOnClickListener {
+            finish()
+        }
+
+
     }
 }
