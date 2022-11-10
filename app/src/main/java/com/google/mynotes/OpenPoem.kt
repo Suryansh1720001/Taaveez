60975ef134161130046a5ef9e733aada1ead1220
Suryansh Prajapati <suryansh7202@gmail.com>
2022-11-10 19:34:26 +0530

    add update image and remove extra openPoem layout

--- app/src/main/java/com/google/mynotes/OpenPoem.kt
+++ app/src/main/java/com/google/mynotes/OpenPoem.kt
@@ -1,17 +1,17 @@
 package com.google.mynotes
 
-import android.app.Dialog
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.mynotes.databinding.ActivityOpenPoemBinding
-import com.google.mynotes.databinding.OpenNotesBinding
-import kotlinx.coroutines.launch
+
 
 class OpenPoem : AppCompatActivity() {
 
-    private var binding :ActivityOpenPoemBinding ?=null
+    private var binding :ActivityOpenPoemBinding?=null
     private var PoemTopic: String? =null
     private var PoemDes: String? =null
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         PoemTopic = intent.getStringExtra(Constants.POEM_TOPIC)
         PoemDes = intent.getStringExtra(Constants.POEM_DES)
@@ -21,14 +21,11 @@ class OpenPoem : AppCompatActivity() {
         setContentView(binding?.root)
 
 
-
-
-
         binding?.tvTopic?.setText(PoemTopic)
         binding?.tvPoemDes?.setText(PoemDes)
 
         binding?.btnClose?.setOnClickListener {
-            onBackPressed()
+           finish()
         }
 
 
